9fd5fdfc90c955bb6fe62db5adcda3d6eddf5bc4
aman-khan-786 <armantechgaming@gmail.com>
2025-10-22 15:46:54 +0530

    Remove unnecessary blank lines in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.appilistpremiumservice"
     compileSdk = 33
-    
+
     defaultConfig {
         applicationId = "com.appilistpremiumservice"
         minSdk = 21
@@ -14,26 +14,26 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    
+
     buildTypes {
         release {
             isMinifyEnabled = false
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
         jvmTarget = "17"
     }
-    
+
     buildFeatures {
         compose = true
     }
-    
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
@@ -42,11 +42,11 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    
+
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
