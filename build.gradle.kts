7fcfe2fa9dbec25e365a8ffe500bed368cf0f6ac
aman-khan-786 <armantechgaming@gmail.com>
2025-10-22 16:06:55 +0530

    Set Kotlin compile options to JVM target 17
    Updated Kotlin compile options to target Java 17.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,25 +5,30 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0") // Latest stable as of Oct 2025
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24") // Latest stable Kotlin
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.4.0" apply false // Updated to match Gradle plugin
-    id("com.android.library") version "8.4.0" apply false    // Updated to match Gradle plugin
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Updated to match Kotlin plugin
+    id("com.android.application") version "8.4.0" apply false
+    id("com.android.library") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-// Optional: Add repositories for all projects
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
