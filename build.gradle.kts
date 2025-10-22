bea4cfaa56841432a1e7feeedf9efea2299070d3
aman-khan-786 <armantechgaming@gmail.com>
2025-10-22 16:00:20 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,25 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.android.tools.build:gradle:8.4.0") // Latest stable as of Oct 2025
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24") // Latest stable Kotlin
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application") version "8.4.0" apply false // Updated to match Gradle plugin
+    id("com.android.library") version "8.4.0" apply false    // Updated to match Gradle plugin
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Updated to match Kotlin plugin
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// Optional: Add repositories for all projects
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
